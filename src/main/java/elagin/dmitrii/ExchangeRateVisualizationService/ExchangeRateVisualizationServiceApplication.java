760e187575a458388b4ac9ab74653afbd2cd7629
Dmitrii Elagin <dmitriielagin21@gmail.com>
2022-06-01 11:46:09 +0300

    Initial commit

---
+++ src/main/java/elagin/dmitrii/ExchangeRateVisualizationService/ExchangeRateVisualizationServiceApplication.java
@@ -0,0 +1,13 @@
+package elagin.dmitrii.ExchangeRateVisualizationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateVisualizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateVisualizationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/elagin/dmitrii/ExchangeRateVisualizationService/ExchangeRateVisualizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package elagin.dmitrii.ExchangeRateVisualizationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateVisualizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
